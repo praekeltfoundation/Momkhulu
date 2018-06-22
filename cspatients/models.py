e792521cb4b474fb2935dadecb3335360fd1df1e
Nyasha <nyashabryan23@gmail.com>
2018-06-22 12:21:25 +0100

    Added a Patient model

--- cspatients/models.py
+++ cspatients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Patient(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    gravidity = models.CharField()
+    comorbidity = models.CharField()
+    indication = models.CharField(max=4, min=1)
+    time = models.TimeField()
+    urgency = models.IntegerField()
+    location = models.CharField()
+    data = models.CharField()
+    clinician = models.CharField()
