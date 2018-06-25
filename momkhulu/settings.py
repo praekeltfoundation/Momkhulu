c5a564048fe65be212e5df462859f46a26e73d5b
Nyasha <nyashabryan23@gmail.com>
2018-06-25 12:01:01 +0100

    Url and Settings additions

--- momkhulu/settings.py
+++ momkhulu/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #CS Patients Logging App
+    # CS Patients Logging App
     'cspatients.apps.CspatientsConfig',
     # The Django Rest FrameWork
     'rest_framework',
