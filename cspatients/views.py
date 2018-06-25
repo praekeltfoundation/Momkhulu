272a48280848658193c295eb72ff7461ffec04f5
Nyasha <nyashabryan23@gmail.com>
2018-06-25 12:00:37 +0100

    Quick fix on the views

--- cspatients/views.py
+++ cspatients/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.db.models.query import EmptyQuerySet
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Patient
+from .forms import PatientLogForm
+
+
+def index(request):
+
+    patients_list = Patient.objects.all()
+    if isinstance(patients_list, EmptyQuerySet):
+        patients_list = False
+    template = loader.get_template('cspatients/index.html')
+    context = {
+        'patients_list': patients_list
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def form(request):
+    return render(request, "cspatients/form.html")
+
+
+def submit(request):
+    if request.method == "POST":
+        form = PatientLogForm(request.POST)
+        if form.is_valid():
+            return render(request, "cspatients/index.html")
+    return redirect("form")
