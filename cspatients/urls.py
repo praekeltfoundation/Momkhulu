8fe3ca03a8807cff998bcfca6cb11880df111eb5
Nyasha <nyashabryan23@gmail.com>
2018-07-09 18:45:55 +0100

    Prefixed all the api urls with /api/

--- cspatients/urls.py
+++ cspatients/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.view, name="root"),
     path('view', views.view, name="view"),
     path('form', views.form, name="form"),
-    path('rpevent', views.rp_event, name='rp_event'),
     path('login', views.log_in, name="log_in"),
     path('logout', views.log_out, name="log_out"),
     path('patient/<str:patient_id>', views.patient, name="patient"),
-    path('patientexists', views.patientexists, name="patientexits"),
+    path('api/rpevent', views.rp_event, name='rp_event'),
+    path('api/patientexists', views.patientexists, name="patientexits"),
 ]
