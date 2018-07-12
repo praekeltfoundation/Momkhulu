b25cf565c6aaa97804c85cba8d50c6f6be645f01
Nyasha <nyashabryan23@gmail.com>
2018-07-12 14:02:58 +0100

    Removed unused get_object_or_404 method

--- cspatients/views.py
+++ cspatients/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
 from datetime import datetime
 
