02ddf37a16c4b6f609de962cbb48441706742c20
Nyasha <nyashabryan23@gmail.com>
2018-07-22 16:54:07 +0100

    Fixed line too long error

--- cspatients/urls.py
+++ cspatients/urls.py
@@ -10,7 +10,15 @@ urlpatterns = [
     path('logout', views.log_out, name="log_out"),
     path('patient/<str:patient_id>', views.patient, name="patient"),
     path('api/rpevent', views.rp_event, name='rp_event'),
-    path('api/rppatientexists', views.rp_patientexists, name="rp_patientexits"),
+    path(
+        'api/rppatientexists',
+        views.rp_patientexists,
+        name="rp_patientexits"
+    ),
     path('api/rpentrychanges', views.rp_entrychanges, name="rp_entrychanges"),
-    path('api/rpentrydelivered', views.rp_entrydelivered, name="rp_entrydelivered")
+    path(
+        'api/rpentrydelivered',
+        views.rp_entrydelivered,
+        name="rp_entrydelivered"
+    )
 ]
