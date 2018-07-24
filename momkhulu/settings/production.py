605556e59f48495ce0b5398a4e23664a40e576a3
Nyasha <nyashabryan23@gmail.com>
2018-07-24 06:19:54 +0100

    Added a few settings in production settings

--- momkhulu/settings/production.py
+++ momkhulu/settings/production.py
@@ -1,1 +1,14 @@
 from .base import * # noqa
+import os
+
+DEBUG = False
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
+
+# PostGres db Setup
+
+DATABASES = {
+
+}
