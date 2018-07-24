539ccce04ecca7b830dbb23ad6319dbe4b29ede1
Nyasha <nyashabryan23@gmail.com>
2018-07-24 16:33:00 +0100

    Added default debug value

--- momkhulu/settings/base.py
+++ momkhulu/settings/base.py
@@ -5,14 +5,11 @@ import environ
 root = environ.Path(__file__) - 3
 BASE_DIR = root()
 
-env = environ.Env()
+env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
