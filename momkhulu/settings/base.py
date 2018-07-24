49ae32856848edcf9938e4dbb0a71024d1b34d6a
Nyasha <nyashabryan23@gmail.com>
2018-07-24 06:14:00 +0100

    Moved the database selection to dev settings file

--- momkhulu/settings/base.py
+++ momkhulu/settings/base.py
@@ -1,7 +1,9 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,13 +77,6 @@ CHANNEL_LAYERS = {
 }
 
 # Database
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
