f7f634297e74528b42a5a36fc1647bee77d700c5
Nyasha <nyashabryan23@gmail.com>
2018-07-24 16:54:28 +0100

    Bug fixes

--- momkhulu/settings/dev.py
+++ momkhulu/settings/dev.py
@@ -6,9 +6,6 @@ import environ
 
 DEBUG = True
 
-root = environ.Path(__file__) - 2
-BASE_DIR = root()
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- momkhulu/settings/production.py
+++ momkhulu/settings/production.py
@@ -1,10 +1,9 @@
 from .base import * # noqa
 from .base import env
-
 # PostGres db Setup
 
 DATABASES = {
 
 }
 
-ALLOWED_HOSTS = env.str("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = env.str("ALLOWED_HOSTS", "").split(",")
