ca9a8119af6e0de62c16f856931f63122b02f504
Nyasha <nyashabryan23@gmail.com>
2018-07-26 06:34:06 +0100

    Changed settings in asgi app to default to settings/dev.py

--- momkhulu/asgi.py
+++ momkhulu/asgi.py
@@ -7,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "momkhulu.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "momkhulu.settings.dev")
 django.setup()
 application = get_default_application()
