9f36d16687bc6ec8d3d7a0f40730e2b67ea77cd4
Erik Harding <erik@praekelt.org>
2019-04-03 22:53:38 +0800

    add sentry

--- momkhulu/settings/production.py
+++ momkhulu/settings/production.py
@@ -3,3 +3,8 @@ from .base import env
 
 SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = env.str("ALLOWED_HOSTS", "").split(",")
+
+# Configure Sentry Logging
+INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
+RAVEN_DSN = env.str("RAVEN_DSN", "")
+RAVEN_CONFIG = {"dsn": RAVEN_DSN} if RAVEN_DSN else {}
