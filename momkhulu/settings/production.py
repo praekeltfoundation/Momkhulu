97fa5203898ecbf3bb9b748797b2ff9553992c72
Erik Harding <erik@praekelt.org>
2019-04-03 15:20:55 +0800

    change dev db to postgres

--- momkhulu/settings/production.py
+++ momkhulu/settings/production.py
@@ -3,8 +3,3 @@ from .base import env
 
 SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = env.str("ALLOWED_HOSTS", "").split(",")
-
-# PostGres db Setup
-DATABASES = {
-    "default": env.db(default="postgres://momkhulu:momkhulu@localhost:5432/momkhulu")
-}
